da8b8703916e87f831b577b3cc5dd5b36a65c257
theluanz <luanzuffo.lz@gmail.com>
2022-08-26 20:14:34 -0300

    Visual Form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
+
+@Injectable()
 export class FormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  form: FormGroup;
+  formValues = {
+    qtdAnimais: 12,
+    consumoGramas: 3000,
+    periodo: 30,
+  }
+  constructor(private _formBuilder: FormBuilder) {
+    this.form = this._formBuilder.group({
+      qtdAnimais: [this.formValues.qtdAnimais],
+      consumoGramas: [this.formValues.consumoGramas],
+      periodo: [this.formValues.periodo],
+    });
   }
 
+  ngOnInit(): void {}
+
+  handleSubmit(event: Event) {
+    event.preventDefault();
+    console.log(this.form.value);
+  }
 }
